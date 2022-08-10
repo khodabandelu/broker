f49511afc393120a93eae022d642efeeb2042ded
Mahdi Khodabandelu <ma.khodabandelu@gmail.com>
2022-08-11 00:38:22 +0430

    register all handlers in application

--- broker-account/account-query/src/main/java/com/macan/account/query/AccountQueryApplication.java
+++ broker-account/account-query/src/main/java/com/macan/account/query/AccountQueryApplication.java
@@ -1,13 +1,32 @@
 package com.macan.account.query;
 
+import com.macan.account.query.api.queries.*;
+import com.macan.cqrs.core.infrastructure.QueryDispatcher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class AccountQueryApplication {
 
+    @Autowired
+    QueryDispatcher queryDispatcher;
+
+    @Autowired
+    QueryHandler queryHandler;
+
     public static void main(String[] args) {
         SpringApplication.run(AccountQueryApplication.class, args);
+    }
+
+    @PostConstruct
+    public void registerHandlers() {
+        queryDispatcher.registerHandler(FindAllAccountsQuery.class, queryHandler::handle);
+        queryDispatcher.registerHandler(FindAccountByIdQuery.class, queryHandler::handle);
+        queryDispatcher.registerHandler(FindAccountByHolderQuery.class, queryHandler::handle);
+        queryDispatcher.registerHandler(FindAccountWithBalanceQuery.class, queryHandler::handle);
     }
 
 }
